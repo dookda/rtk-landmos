a7c2beb5ab53c2bf884a3592d42b7c9c0c561711
sakda homhuan <sakda.homhuan@gmail.com>
2022-11-09 17:17:48 +0700

    update

---
+++ webapp/src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Card = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    )
+}
